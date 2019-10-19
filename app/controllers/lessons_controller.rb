b4ca888e90d88403128bf1abd70eef02da2361b3
Brandon Sullivan <jbs81890@gmail.com>
2019-10-19 19:18:29 +0000

    Added permissions for students to access lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,19 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
+
   def show
   end
 
+
+
   private
+
+  def require_authorized_for_current_lesson
+    unless current_user.enrolled_in?(current_lesson.section.course) != current_user
+    redirect_to course_path, alert: 'You must be enrolled to view this lesson'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
